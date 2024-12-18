34e4daa1b776e2c23ef2720f2424317b0b5f258d
Tanzim Hossain Romel <romel.rcs@gmail.com>
2024-12-18 12:15:48 +0600

    Add CI/CD

--- tests/OldPhoneKeypadDecoder.Tests/obj/Debug/net8.0/OldPhoneKeypadDecoder.Tests.AssemblyInfo.cs
+++ tests/OldPhoneKeypadDecoder.Tests/obj/Debug/net8.0/OldPhoneKeypadDecoder.Tests.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("OldPhoneKeypadDecoder.Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2052e1dd446f36496f1078ea2633400393b227a1")]
 [assembly: System.Reflection.AssemblyProductAttribute("OldPhoneKeypadDecoder.Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("OldPhoneKeypadDecoder.Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
